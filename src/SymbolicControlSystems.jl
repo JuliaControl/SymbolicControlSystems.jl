06e8207073f73cead3779a826e00f468b01cbf6c
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2020-10-23 15:17:35 +0200

    implement stuff

--- src/SymbolicControlSystems.jl
+++ src/SymbolicControlSystems.jl
@@ -1,5 +1,88 @@
 module SymbolicControlSystems
+using LinearAlgebra
+using ControlSystems, SymPy, Latexify
 
-# Write your package code here.
+export sp, sym2num, latextf, Sym, @vars, simplify
+
+const sp = SymPy.PyCall.PyNULL()
+
+
+function __init__()
+    @eval global const s = Sym("s")
+    copy!(sp, SymPy.sympy)
+end
+
+function SymPy.Sym(sys::StateSpace{<:Any, Sym})
+    A,B,C,D = ControlSystems.ssdata(sys)
+    simplify((C*inv(s*I(size(A,1)) - A)*B + D)[1])
+end
+
+function ControlSystems.tf(sys::Sym)
+    sys = simplify(sys)
+    sp.monic(inv(sys)) |> inv |> simplify # makes the denominator monic
+    n,d = sp.fraction(sys) .|> sp.Poly
+    tf(expand_coeffs(n), expand_coeffs(d))
+end
+
+function expand_coeffs(n; numeric=false)
+    n = sp.Poly(n,s)
+    deg = n.degree() |> Float64 |> Int
+    c = n.coeffs() # This fails if the coeffs are symbolic
+    numeric && (c = Float64.(c))
+    [c; zeros(eltype(c), deg-length(c)+1)]
+end
+expand_coeffs(n::Real; numeric=false) = n
+
+
+function ControlSystems.minreal(sys::TransferFunction{<:Any, <:ControlSystems.SisoTf{<:Sym}})
+    Sym(sys) |> simplify |> tf
+end
+
+
+function sym2num(P, ps, p)
+    @warn "TODO: accept a vector of pairs instead"
+    # for i = 1:2
+    for sym = ps
+        ssym = Symbol(sym)
+        hasfield(typeof(p), ssym) || continue
+        val = float(getfield(p, ssym))
+        P = subs(P, (sym, val))
+    end
+    # end
+    n, d = sp.fraction(P) .|> sp.Poly
+    nn = expand_coeffs(n, numeric=true)
+    nd = expand_coeffs(d, numeric=true)
+    G = tf(nn, nd isa Number ? [nd] : nd)
+    # isstable(G) || @error "Unstable transfer function with poles $(pole(G))"
+    G
+end
+
+
+function sym2num(P, pairs::Pair...)
+    # for i = 1:2
+    for (sym, val) = pairs
+        ssym = Symbol(sym)
+        P = subs(P, (sym, val))
+    end
+    n, d = sp.fraction(P) .|> sp.Poly
+    nn = expand_coeffs(n)
+    nd = expand_coeffs(d)
+    G = tf(nn, nd isa Number ? [nd] : nd)
+
+    G
+end
+
+function latextf(x)
+    @warn "TODO: group coefficients of powers of s together"
+    n,d = sp.fraction(x)
+    s = "\$\\dfrac{$((n))}{$((d))}\$"
+    s = replace(s, '*' => "")
+    clipboard(s)
+    s
+end
+
+# replace_controllers(ex) = subs(ex, (C, Cᵥ2*(s+Cₚ2)), (Cₚ, Cₚ2), (Cᵥ,Cᵥ2)) |> simplify
+# numeric_tf(ex) = sym2num(replace_controllers(ex), ps, pn)
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,28 @@
 using SymbolicControlSystems
 using Test
+using ControlSystems
+using SymbolicControlSystems: s
+
 
 @testset "SymbolicControlSystems.jl" begin
-    # Write your tests here.
+    @testset "To symbolic" begin
+        @info "Testing To symbolic"
+
+
+        pol = 2s^2+3s+4
+        @test SymbolicControlSystems.expand_coeffs(pol) == [2,3,4]
+
+        pol = 2s^3+3s^2+4*s
+        @test SymbolicControlSystems.expand_coeffs(pol) == [2,3,4,0]
+
+        @vars ω0
+        sys = ControlSystems.DemoSystems.resonant(;ω0)
+        ssys = Sym(sys)
+        n, d = sp.fraction(simplify(ssys))
+        @test SymbolicControlSystems.expand_coeffs(n) == [ω0^3]
+        @test sum(SymbolicControlSystems.expand_coeffs(d) - [1, 0.5, ω0^2+0.25^2]) == 0
+
+
+
+    end
 end
