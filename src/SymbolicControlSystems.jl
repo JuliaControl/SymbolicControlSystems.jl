dbd69a76e396768f8bc46aecc0dd0c11756d4c7c
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2021-06-05 07:10:31 +0200

    formatting changes

--- src/SymbolicControlSystems.jl
+++ src/SymbolicControlSystems.jl
@@ -15,20 +15,20 @@ function __init__()
 end
 
 
-function SymPy.Sym(sys::StateSpace{<:Any, Sym})
-    A,B,C,D = ControlSystems.ssdata(sys)
+function SymPy.Sym(sys::StateSpace{<:Any,Sym})
+    A, B, C, D = ControlSystems.ssdata(sys)
     if isdiscrete(sys)
-        (C*inv(z*I(size(A,1)) - A)*B + D)[1]
+        (C*inv(z * I(size(A, 1)) - A)*B+D)[1]
     else
-        (C*inv(s*I(size(A,1)) - A)*B + D)[1]
+        (C*inv(s * I(size(A, 1)) - A)*B+D)[1]
     end
 end
 
 function SymPy.Sym(sys::TransferFunction)
     if isdiscrete(sys)
-        ssys = sp.Poly(numvec(sys)[], z)/sp.Poly(denvec(sys)[], z)
+        ssys = sp.Poly(numvec(sys)[], z) / sp.Poly(denvec(sys)[], z)
     else
-        ssys = sp.Poly(numvec(sys)[], s)/sp.Poly(denvec(sys)[], s)
+        ssys = sp.Poly(numvec(sys)[], s) / sp.Poly(denvec(sys)[], s)
     end
 end
 
@@ -37,8 +37,8 @@ end
 
 Convert a symbolic, rational expression into a transfer function. `h` denotes the sample time if the system is discrete.
 """
-function ControlSystems.tf(sys::Sym, h=nothing)
-    n,d = sp.fraction(sys)
+function ControlSystems.tf(sys::Sym, h = nothing)
+    n, d = sp.fraction(sys)
     d = sp.Poly(d)
     # d = d.monic() # Don't do this here
     n = n isa Number ? n : sp.Poly(n)
@@ -49,23 +49,23 @@ function ControlSystems.tf(sys::Sym, h=nothing)
     end
 end
 
-function expand_coeffs(n, var; numeric=false)
-    n = sp.Poly(n,var)
+function expand_coeffs(n, var; numeric = false)
+    n = sp.Poly(n, var)
     deg = n.degree() |> Float64 |> Int
     c = n.all_coeffs() # This fails if the coeffs are symbolic
     numeric && (c = Float64.(c))
-    [c; zeros(eltype(c), deg-length(c)+1)]
+    [c; zeros(eltype(c), deg - length(c) + 1)]
 end
-expand_coeffs(n::Real, args...; numeric=false) = n
+expand_coeffs(n::Real, args...; numeric = false) = n
 
 
-function ControlSystems.minreal(sys::TransferFunction{<:Any, <:ControlSystems.SisoTf{<:Sym}})
+function ControlSystems.minreal(sys::TransferFunction{<:Any,<:ControlSystems.SisoTf{<:Sym}})
     Sym(sys) |> simplify |> tf
 end
 
-function ControlSystems.tf(sys::StateSpace{<:Any, Sym})
-    n,p = simplify.(sp.Poly.(simplify.(sp.fraction(simplify(Sym(sys)))), s))
-    tf(simplify(n/p))
+function ControlSystems.tf(sys::StateSpace{<:Any,Sym})
+    n, p = simplify.(sp.Poly.(simplify.(sp.fraction(simplify(Sym(sys)))), s))
+    tf(simplify(n / p))
 end
 
 
@@ -77,15 +77,17 @@ sym2num(P::TransferFunction, args...) = sym2num(Sym(P), args...)
 Replace symbols by numbers. `h` indicates the sample rate if the system is discrete.
 """
 function sym2num(P::Sym, h::Real, pairs::Pair...)
-    P.has(s) && error("Found `z` in symbolic expression, provide sample time as second argument to `sym2num`")
-    for (sym, val) = pairs
+    P.has(s) && error(
+        "Found `z` in symbolic expression, provide sample time as second argument to `sym2num`",
+    )
+    for (sym, val) in pairs
         P = subs(P, (sym, val))
     end
     n, d = sp.Poly.(sp.fraction(P), z)
     nn = float.(expand_coeffs(n, z))
     nd = float.(expand_coeffs(d, z))
     if nd[1] != 1 # make monic
-        nn,nd = monic(nn,nd)
+        nn, nd = monic(nn, nd)
     end
     G = tf(nn, nd isa Number ? [nd] : nd, h)
     G
@@ -99,16 +101,18 @@ Replace symbols by numbers.
 """
 function sym2num(P::Sym, pairs::Pair...)
     # for i = 1:2
-    P.has(z) && error("Found `z` in symbolic expression, provide sample time as second argument to `sym2num`")
+    P.has(z) && error(
+        "Found `z` in symbolic expression, provide sample time as second argument to `sym2num`",
+    )
     P.has(s) || error("Found no `s` in symbolic expression, provide")
-    for (sym, val) = pairs
+    for (sym, val) in pairs
         P = subs(P, (sym, val))
     end
-    n, d = sp.Poly.(sp.fraction(P), s)    
+    n, d = sp.Poly.(sp.fraction(P), s)
     nn = float.(expand_coeffs(n, s))
     nd = float.(expand_coeffs(d, s))
     if nd[1] != 1 # make monic
-        nn,nd = monic(nn,nd)
+        nn, nd = monic(nn, nd)
     end
     G = tf(nn, nd isa Number ? [nd] : nd)
     G
@@ -127,16 +131,16 @@ end
 
 function tustin(Gs::Sym, h)
     hh = Sym("hh")
-    Gt = Gs.subs(s, hh * (z-1)/(z+1)) |> simplify
+    Gt = Gs.subs(s, hh * (z - 1) / (z + 1)) |> simplify
     @info "Perforing pole-zero cancellation (this might take a while)"
     @time Gt = sp.cancel(Gt) # Simplify takes forever with floating point values
     @time Gt = sp.collect(Gt, z) # Simplify takes forever with floating point values
-    Gt = Gt.subs(hh, (2/h))
+    Gt = Gt.subs(hh, (2 / h))
     Gtf = tf(Gt, h)
-    n,d = numvec(Gtf)[], denvec(Gtf)[]
+    n, d = numvec(Gtf)[], denvec(Gtf)[]
     d[1] == 1 && (return Gtf)
-    n,d = monic(n,d)
-    tf(n,d,h)
+    n, d = monic(n, d)
+    tf(n, d, h)
 end
 
 """
@@ -155,10 +159,10 @@ bodeplot!(doubleeuler(sys, 0.001), w, label="double euler")
 """
 function doubleeuler(sys::AbstractStateSpace{<:Continuous}, Ts)
     A, B, C, D = ssdata(sys)
-    T = promote_type(eltype.((A,B,C,D))...)
+    T = promote_type(eltype.((A, B, C, D))...)
     ny, nu = size(sys)
     nx = sys.nx
-    Ad, Bd, Cd, Dd = (I + Ts*A + Ts^2/2*A^2), Ts*B, C, D
+    Ad, Bd, Cd, Dd = (I + Ts * A + Ts^2 / 2 * A^2), Ts * B, C, D
     ss(Ad, Bd, Cd, Dd, Ts)
 end
 
@@ -173,11 +177,11 @@ Return a string with C-code for filtering a signal `u` through `G`.
 - `simplify`: A function for symbolic simplification. You may try `Sympy.simplify`, but for large systems, this will take a long time to compute.
 - `cse`: Perform common subexpression elimination. This generally improvems the performance of the generated code.
 """
-function ccode(G::TransferFunction; simplify = identity, cse=true)
+function ccode(G::TransferFunction; simplify = identity, cse = true)
     P = Sym(G)
     P.has(z) || error("Did not find `z` in symbolic expression")
     P.has(s) && error("Found `s` in symbolic expression, provide expression in `z`")
-    n,d = numvec(G)[], denvec(G)[]
+    n, d = numvec(G)[], denvec(G)[]
     if d[1] != 1
         @info "Denominator polynomial not monic, dividing by the leading coefficient."
         n = simplify.(n ./ d[1])
@@ -187,13 +191,13 @@ function ccode(G::TransferFunction; simplify = identity, cse=true)
     @info vars = P.free_symbols
     vars.remove(z)
     vars = collect(vars)
-    vars = sort(vars, by=string)
+    vars = sort(vars, by = string)
     var_str = ""
     for var in vars
         var_str *= ", double $(var)"
     end
 
-    nu,ny = length(n), length(d)
+    nu, ny = length(n), length(d)
     code = """
 #include <stdio.h>\n
 #include <math.h>\n
@@ -211,8 +215,8 @@ double transfer_function(double ui$(var_str)) {
     y[0] = 0;
 """
     if cse
-    @info "Finding common subexpressions"
-        subex, final = sp.cse([n;d])
+        @info "Finding common subexpressions"
+        subex, final = sp.cse([n; d])
         if length(final) == 1
             final = final[]
         end
@@ -222,11 +226,11 @@ double transfer_function(double ui$(var_str)) {
             code *= "    double $(se[1]) = $(sp.ccode(se[2]));\n"
         end
     end
-    for (i,n) in enumerate(n)
+    for (i, n) in enumerate(n)
         @info "Writing numerator term $i/$(length(n))"
         code *= "    y[0] += ($(sp.ccode(n)))*u[$(i-1)];\n"
     end
-    for (i,n) in enumerate(d[2:end])
+    for (i, n) in enumerate(d[2:end])
         @info "Writing denominator term $i/$(length(d)-1)"
         code *= "    y[0] += ($(sp.ccode(-n)))*y[$(i)];\n"
     end
@@ -236,19 +240,19 @@ double transfer_function(double ui$(var_str)) {
     code
 end
 
-function ccode(sys::StateSpace{<:Discrete}; cse=true, function_name = "transfer_function")
+function ccode(sys::StateSpace{<:Discrete}; cse = true, function_name = "transfer_function")
     sys.nu == 1 || throw(ArgumentError("Multiple input not yet supported"))
     nx = sys.nx
     ny = sys.ny
     u = Sym("u")
-    x = [Sym("x[$(i-1)]") for i in 1:nx]
+    x = [Sym("x[$(i-1)]") for i = 1:nx]
     # @show P
     if ControlSystems.numeric_type(sys) <: SymPy.Sym
         P = Sym(sys)
         vars = P.free_symbols
         vars.remove(z)
         vars = collect(vars)
-        vars = sort(vars, by=string)
+        vars = sort(vars, by = string)
         var_str = ""
         for var in vars
             var_str *= ", double $(var)"
@@ -258,10 +262,10 @@ function ccode(sys::StateSpace{<:Discrete}; cse=true, function_name = "transfer_
     end
     x1 = zeros(Sym, nx) # workaround for strange bug with undefinied referece appearing in Pluto only
     y = zeros(Sym, ny)
-    @show x1 = mul!(x1, sys.A, x) + sys.B*u
-    @show y = mul!(y, sys.C, x) + sys.D*u
+    @show x1 = mul!(x1, sys.A, x) + sys.B * u
+    @show y = mul!(y, sys.C, x) + sys.D * u
     # @show y = sp.collect.(y, x)
-    
+
     code = """
 #include <stdio.h>\n
 #include <math.h>\n
@@ -272,7 +276,7 @@ void $(function_name)(double *y, double u$(var_str)) {
 """
     if cse
         @info "Finding common subexpressions"
-        subex, final = sp.cse([x1;y])
+        subex, final = sp.cse([x1; y])
         x1 = final[][1:length(x1)]
         y = final[][length(x1)+1:end]
         for se in subex
@@ -280,7 +284,7 @@ void $(function_name)(double *y, double u$(var_str)) {
         end
     end
     code *= "\n    // Advance the state xp = Ax + Bu\n"
-    for (i,n) in enumerate(x1)
+    for (i, n) in enumerate(x1)
         code *= "    xp[$(i-1)] = ($(sp.ccode(n)));\n"
     end
     code *= """
@@ -288,7 +292,7 @@ void $(function_name)(double *y, double u$(var_str)) {
         // Accumulate the output y = C*x + D*u
     """
 
-    for (i,n) in enumerate(y)
+    for (i, n) in enumerate(y)
         code *= "    y[$(i-1)] = ($(sp.ccode(n)));\n"
     end
     code *= """
@@ -310,7 +314,7 @@ function structured_text(code)
 
     code = replace(code, "pow" => "EXPT")
     code = replace(code, "static " => "")
-    
+
     code = replace(code, r"double (\w+) =" => s"\1 : LREAL :=")
     code = replace(code, r"double (\w+)" => s"\1 : LREAL")
     code = replace(code, r"(.{1,20}?) \+= " => s"\1 := \1 + ")
@@ -325,13 +329,13 @@ latextf(x::TransferFunction, args...) = latextf(Sym(x), args...)
 
 Latexify.Latexify(x::TransferFunction) = latextf(x, args...)
 
-function monic(n,d) 
+function monic(n, d)
     n = n ./ d[1]
-    d = d ./ d[1] 
+    d = d ./ d[1]
     if n isa Number
         n = [n]
     end
-    n,d
+    n, d
 end
 
 """
@@ -339,13 +343,13 @@ end
 
 Return a latex string representing `x`. If `mon`, then the denominator polynomial will be made monic (leading coefficient = 1).
 """
-function latextf(x::Sym, mon=true)
+function latextf(x::Sym, mon = true)
     var = x.has(z) ? z : s
-    n,d = sp.fraction(x)
+    n, d = sp.fraction(x)
     if mon
-        n,d = monic(n,d)
+        n, d = monic(n, d)
     end
-    n,d = sp.collect.((n,d), var)
+    n, d = sp.collect.((n, d), var)
     str = "\$\\dfrac{$((n))}{$((d))}\$"
     str = replace(str, '*' => "")
     clipboard(str)
@@ -377,7 +381,7 @@ sys2vec = @(sys) [
         sys.D(:)
     ]
 """
-function vec2sys(v, ts=nothing)
+function vec2sys(v, ts = nothing)
     nx = Int(v[1])
     nu = Int(v[2])
     ny = Int(v[3])
@@ -389,7 +393,7 @@ function vec2sys(v, ts=nothing)
     B = reshape(v[bi], nx, nu)
     C = reshape(v[ci], ny, nx)
     D = reshape(v[di], ny, nu)
-    ts === nothing ? ss(A,B,C,D) : ss(A,B,C,D, ts)
+    ts === nothing ? ss(A, B, C, D) : ss(A, B, C, D, ts)
 end
 
 
