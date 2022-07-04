b1871aebcd9f1c310061c5776d87862c97b72827
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2022-07-04 11:00:37 +0200

    add docs for interpolated systems

--- src/SymbolicControlSystems.jl
+++ src/SymbolicControlSystems.jl
@@ -556,6 +556,12 @@ end
 
 
 # Interpolation
+
+"""
+    print_c_array(io, a::Vector{<:AbstractArray}, t::AbstractVector, name = "mat"; cse = false, s = "", print_vector = true, print_logic = true, struct_name::Union{Nothing, String} = nothing, struct_type = nothing, ivecname = name * "_interp_vect")
+
+Write C-code for interpolating between arrays `a`. The array `t` contains the interpolation points.
+"""
 function print_c_array(
     io,
     a::Vector{<:AbstractArray},
@@ -644,6 +650,15 @@ end
 
 
 # Interpolated StateSpace
+"""
+    print_c_array(io, sys::Vector{<:AbstractStateSpace}, t::AbstractVector, name = "sys"; cse = false, s = "", en = "", struct_name::Union{Nothing, String} = nothing, struct_type = nothing)
+
+Write C-code for an interpolated linear system. The interpolation vector `t` defines the interpolation points, this vector is expected to be of the same length as the vector of linear systems `sys`. 
+
+- `s, en`: are strings that are appended at the start and end of variables names in the C-code.
+- `struct_name`: If provided, the interpolation matrices will be placed inside a struct with this name.
+- `struct_type`: If the struct name is used, provide also the C type of the struct.
+"""
 function print_c_array(
     io,
     sys::Vector{<:AbstractStateSpace},
